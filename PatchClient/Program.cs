a9af43a292b2df7b3ffc8bcae4b45e1b19ee47a0
xHeinrich <nathan_heinrich@outlook.com>
2016-01-09 23:22:22 -0700

    Updated Documentation

--- PatchClient/Program.cs
+++ PatchClient/Program.cs
@@ -10,18 +10,26 @@ namespace PatchClient
         static void Main(string[] args)
         {
             // Variables needed to create patch
+
+            /*
             Gryffi.FTPPassword = "";
             Gryffi.FTPUsername = "";
             Gryffi.FTPUrl = "";
             Gryffi.PatchServerUrl = "";
             Gryffi.PatchFolder = @"C:\Users\Administrator\Documents\visual studio 2015\Projects\PatchClient\PatchClient\bin\Patch";
-
+            */
+            Gryffi.FTPPassword = "test1234.";
+            Gryffi.FTPUsername = "patchclient@nathan-dev.com";
+            Gryffi.FTPUrl = "ftp://nathan-dev.com";
+            Gryffi.PatchServerUrl = "http://www.nathan-dev.com/projects/patchclient/";
+            Gryffi.PatchFolder = @"C:\Users\Administrator\Documents\visual studio 2015\Projects\PatchClient\PatchClient\bin\Patch";
+            
             //If being patcher
             // ReadPatchlist();
             //Patch();
             //CreateRemoteFolders();
             //If being patchlist creator
-            Gryffi.CreatePatchlist(2, true, Channel.Release);
+            Gryffi.CreatePatchlist(9, true, true, Channel.Release);
             Console.ReadLine();
         }
         
